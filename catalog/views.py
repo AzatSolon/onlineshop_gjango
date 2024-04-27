e2d5e088b3f79368f9325e6fe96288da364294c5
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-04-27 14:36:17 +0300

    rework#3

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'catalog/home_page.html')
+    return render(request, 'home_page.html')
 
 
 def contacts(request):
@@ -12,4 +12,4 @@ def contacts(request):
         message = request.POST.get('message')
         print(f"\nИмя - {name}\nТелефон - {phone}\nСообщение - {message}")
 
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'contacts.html')
