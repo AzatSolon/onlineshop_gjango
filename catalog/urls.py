ee09d8657547f3df2fa87b999e700e9f4dcec21d
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-04-27 14:07:58 +0300

    rework#2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from catalog.apps import CatalogConfig
 from catalog.views import home_page, contacts
 
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home_page, 'home_page'),
-    path('contacts/', contacts, 'contacts'),
+    path('', home_page, name='home_page'),
+    path('contacts/', contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    return render(request, 'catalog/home_page.html')
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"\nИмя - {name}\nТелефон - {phone}\nСообщение - {message}"
+
+        )
+
+    return render(request, 'catalog/contacts.html')
