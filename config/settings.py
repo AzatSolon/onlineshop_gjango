7a0883d4344124123e7670d0b6f4609439d130a0
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-05-09 10:38:58 +0300

    rework

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,4 +89,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
