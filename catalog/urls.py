05183934c65a408f8ead83f7ecf0aaf573da64f8
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-05-10 13:30:51 +0300

    reword settings and views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home_page, contacts
-
+from catalog.views import index
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
+    path('', index),
+    path('home_page/', home_page, name='home_page'),
     path('contacts/', contacts, name='contacts'),
 ]
