cc0c40094a66823a2f6cb09383abdf4aee169b4d
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-05-21 13:51:57 +0300

    fbv -> cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, index, products_list, product_info
+from catalog.views import ProductListView, ProductDeleteView, ProductDetailView, ContactsTemplateView
+
 app_name = CatalogConfig.name
 
+
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts, name='contacts'),
-    path('base/', products_list, name='base'),
-    path('products/<int:pk>/', product_info, name='product_info')
+    path('', ProductListView.as_view(), name='products_list'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    # path('base/', products_list, name='base'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('...', ProductDeleteView.as_view(), name='product_delete')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, TemplateView
 
 from catalog.models import Product
 
 
-def contacts(request):
-    if request.method == 'POST':
+class ContactsTemplateView(TemplateView):
+    template_name = 'contacts.html'
+
+    def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f"\nИмя - {name}\nТелефон - {phone}\nСообщение - {message}")
-
-    return render(request, 'contacts.html')
-
-
-def index(request):
-    return render(request, 'base.html')
+        print(name, phone, message)
+        return super().get(request, *args, **kwargs)
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'product_list.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
-def product_info(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'product_info.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'category', 'price', 'data_make', 'data_last_save')
+    success_url = reverse_lazy('catalog:base')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
