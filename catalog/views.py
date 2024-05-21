9b0b7f9f1576c8cffa35aee459a84fad31489187
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-05-21 15:34:04 +0300

    rework contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, TemplateView
+from pytils.translit import slugify
 
 from catalog.models import Product
+import csv
 
 
 class ContactsTemplateView(TemplateView):
     template_name = 'contacts.html'
 
     def post(self, request, *args, **kwargs):
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(name, phone, message)
-        return super().get(request, *args, **kwargs)
+        name = request.POST.get('name', '')
+        phone = request.POST.get('phone', '')
+        message = request.POST.get('message', '')
+        print(f'{name} ({phone}) написал: {message}')
+
+        with open('contact_info.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([name, phone, message])
+
+        return HttpResponseRedirect(self.request.path)
 
 
 class ProductListView(ListView):
@@ -25,8 +33,16 @@ class ProductDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ('name', 'description', 'image', 'category', 'price', 'data_make', 'data_last_save')
+    fields = ['name', 'description', 'image', 'category', 'price', 'data_make', 'data_last_save']
     success_url = reverse_lazy('catalog:product_list ')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.name)
+            new_mat.save()
+
+        return super().form_valid(form)
 
 
 class ProductDeleteView(DeleteView):
