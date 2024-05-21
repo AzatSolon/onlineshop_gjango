be3b5412da3e781879e12ae416083e3de9e3a724
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-05-21 18:56:30 +0300

    rework blog

--- blog/views.py
+++ blog/views.py
@@ -1,60 +1,48 @@
-from pytils.translit import slugify
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
-
-
-# Create your views here.
-class BlogCreateView(CreateView):
-    model = Blog
-    fields = ('title', 'slug', 'content', 'preview', 'created_at')
-    success_url = reverse_lazy('blog:blog_list')
-
-    def form_valid(self, form):
-        if form.is_valid():
-            new_mat = form.save()
-            new_mat.slug = slugify(new_mat.title)
-            new_mat.save()
-        return super().form_valid(form)
-
-
-class BlogUpdateView(UpdateView):
-    model = Blog
-    fields = ('title', 'slug', 'content', 'preview', 'created_at')
-
-    def form_valid(self, form):
-        if form.is_valid():
-            new_mat = form.save()
-            new_mat.slug = slugify(new_mat.title)
-            new_mat.save()
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('blog:list', args=[self.kwargs.get('pk')])
 
 
 class BlogListView(ListView):
     model = Blog
 
-    def get_queryset(self, *args, **kwargs):
+    def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset.filter(published=True)
-        return queryset
+        return queryset.filter(is_published=True)
 
 
 class BlogDetailView(DetailView):
     model = Blog
 
-    def __init__(self, **kwargs):
-        super().__init__(kwargs)
-        self.object = None
-
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
-        self.object.views += 1
+        self.object.views_count += 1
         self.object.save()
         return self.object
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'is_published']
+    success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_name = form.save()
+            new_name.slug = slugify(new_name.title)
+            new_name.save()
+
+        return super().form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'is_published']
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
