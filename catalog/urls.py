074203ab0203627b06e9a14832f6ccf43a74e3dc
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-05-28 11:28:22 +0300

    add product_update

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDeleteView, ProductDetailView, ContactsTemplateView, ProductCreateView
+from catalog.views import ProductListView, ProductDeleteView, ProductDetailView, ContactsTemplateView, \
+    ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('catalog/create/', ProductCreateView.as_view(), name='products_create'),
     path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
 ]
