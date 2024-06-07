c03574a4b602f882c00946a4bb9bba75d5a0619f
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-06-07 18:03:06 +0300

    rework users 2

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,15 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UserConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, PassRecovery
 
 app_name = UserConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('login/', LoginView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('password-recovery/', PassRecovery.as_view(), name='password-recovery')
 ]
