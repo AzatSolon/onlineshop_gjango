59e6066e5f1212f7f0b0d606469d4163a5200769
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-06-09 17:16:40 +0300

    add verification

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=40, verbose_name='Телефон', help_text='Введите номер', **NULLABLE)
     avatar = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', **NULLABLE)
     country = models.CharField(max_length=80, verbose_name='Страна', **NULLABLE)
+    token = models.CharField(max_length=100, verbose_name='token', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -1,16 +1,32 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+import secrets
+
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
-from users.forms import UserProfileForm, UserPassRecoveryForm
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserProfileForm, UserPassRecoveryForm, UserRegisterForm
 from users.models import User
 
 
 class RegisterView(CreateView):
     model = User
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        token = secrets.token_hex(16)
+        user.token = token
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}'
+        send_mail(subject='Подтверждение почты',
+                  message=f'Подтвердите почту{url}',
+                  from_email=EMAIL_HOST_USER,
+                  recipient_list=[user.email])
+        return super().form_valid(form)
 
 
 class ProfileView(UpdateView):
@@ -27,3 +43,14 @@ class PassRecovery(UpdateView):
     form_class = UserPassRecoveryForm
     template_name = 'users/pass_recovery.html'
     success_url = reverse_lazy('users:login')
+
+
+def email_verification(token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
+
+
+def send_message(request):
+    return render(request, 'users/send_message.html')
